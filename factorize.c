238d589a2c6079deafaff718191a6df00c8028da
2Hasan2 <h01554176846@gmail.com>
2024-01-19 01:52:59 +0200

    done factoriztion

--- factorize.c
+++ factorize.c
@@ -1,35 +1,55 @@
 #include <stdio.h>
 
-void factorize(int n) {
-    int i;
-    for (i = 2; i <= n / 2; i++) {
-        if (n % i == 0) {
-            printf("%d=%d*%d\n", n, i, n / i);
-            return;
-        }
-    }
-    printf("%d=%d*%d\n", n, 1, n);
+/**
+ * factorize - Factorize a number
+ * @n: The number to factorize
+ * Return: Nothing
+ */
+void factorize(long n)
+{
+	long i;
+
+	printf("%ld=", n);
+
+	for (i = 2; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			printf("%ld*%ld\n", n / i, i);
+			return;
+		}
+	}
+	printf("%ld*%ld\n", 1L, n);
 }
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: The command line arguments
+ * Return: 0 if successful, otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	long number;
 
-int main(int argc, char *argv[]) {
-    FILE *file;
-    int number;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return (1);
+	}
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		return (1);
+	}
 
-    file = fopen(argv[1], "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
+	while (fscanf(file, "%ld", &number) == 1)
+	{
+		factorize(number);
+	}
 
-    while (fscanf(file, "%d", &number) == 1) {
-        factorize(number);
-    }
-
-    fclose(file);
-    return 0;
+	fclose(file);
+	return (0);
 }
